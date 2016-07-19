a8b85c5386a36e94e32747b20eaa00118ea4db93
Nikolay Tsonev <nikolay.tsonev@telerik.com>
2016-07-19 13:04:00 +0300

    add list view to second page

--- app/pages.js
+++ app/pages.js
@@ -1,6 +1,13 @@
 "use strict";
 var core_1 = require('@angular/core');
 var router_1 = require('nativescript-angular/router');
+var DataItem = (function () {
+    function DataItem(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return DataItem;
+}());
 var FirstComponent = (function () {
     function FirstComponent() {
     }
@@ -17,12 +24,22 @@ var FirstComponent = (function () {
 exports.FirstComponent = FirstComponent;
 var SecondComponent = (function () {
     function SecondComponent() {
+        this.myItems = [];
+        this.counter = 0;
+        for (var i = 0; i < 50; i++) {
+            this.myItems.push(new DataItem(i, "data item " + i));
+            this.counter = i;
+        }
     }
+    SecondComponent.prototype.onItemTap = function (args) {
+        console.log("------------------------ ItemTapped: " + args.index);
+    };
     SecondComponent = __decorate([
         core_1.Component({
             selector: "second",
             directives: [router_1.NS_ROUTER_DIRECTIVES],
-            template: "\n    <StackLayout>\n        <Label text=\"Second component\" class=\"title\"></Label>\n        <Button text=\"GO TO FIRST\" [nsRouterLink]=\"['/first']\" class=\"link\"></Button>\n    </StackLayout>"
+            template: "\n    <StackLayout>\n        <Label text=\"Second component\" class=\"title\"></Label>\n        <Button text=\"GO TO FIRST\" [nsRouterLink]=\"['/first']\" class=\"link\"></Button>\n        <ListView [items]=\"myItems\" (itemTap)=\"onItemTap($event)\">\n            <template let-item=\"item\" let-i=\"index\" let-odd=\"odd\" let-even=\"even\">\n                <StackLayout [class.odd]=\"odd\" [class.even]=\"even\">\n                    <Label [text]='\"index: \" + i'></Label>\n                    <Label [text]='\"[\" + item.id +\"] \" + item.name'></Label>\n                </StackLayout>\n            </template>\n        </ListView>\n    </StackLayout>",
+            changeDetection: core_1.ChangeDetectionStrategy.OnPush
         }), 
         __metadata('design:paramtypes', [])
     ], SecondComponent);
--- app/pages.ts
+++ app/pages.ts
@@ -1,6 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input, ChangeDetectionStrategy} from '@angular/core';
 import {NS_ROUTER_DIRECTIVES} from 'nativescript-angular/router';
 import {APP_ROUTER_PROVIDERS} from "./app.routes"
+
+class DataItem {
+    constructor(public id: number, public name: string) { }
+}
+
 
 @Component({
     selector: "first",
@@ -20,6 +25,33 @@ export class FirstComponent { }
     <StackLayout>
         <Label text="Second component" class="title"></Label>
         <Button text="GO TO FIRST" [nsRouterLink]="['/first']" class="link"></Button>
-    </StackLayout>`
+        <ListView [items]="myItems" (itemTap)="onItemTap($event)">
+            <template let-item="item" let-i="index" let-odd="odd" let-even="even">
+                <StackLayout [class.odd]="odd" [class.even]="even">
+                    <Label [text]='"index: " + i'></Label>
+                    <Label [text]='"[" + item.id +"] " + item.name'></Label>
+                </StackLayout>
+            </template>
+        </ListView>
+    </StackLayout>`,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SecondComponent { }
+export class SecondComponent { 
+
+  public myItems: Array<DataItem>;
+    private counter: number;
+
+    constructor() {
+        this.myItems = [];
+        this.counter = 0;
+        for (var i = 0; i < 50; i++) {
+            this.myItems.push(new DataItem(i, "data item " + i));
+            this.counter = i;
+        }
+    }
+
+    public onItemTap(args) {
+        console.log("------------------------ ItemTapped: " + args.index);
+    }
+
+}
